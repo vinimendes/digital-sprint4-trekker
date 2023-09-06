466fdacd22f23681f543412f423fab81a2423aa1
Vinicius Medeiros <vini9797@hotmail.com>
2023-09-06 15:46:28 -0300

    Compatibilidade com SQL Server

--- src/test/java/br/fiap/com/Sprint/SprintApplicationTests.java
+++ src/test/java/br/fiap/com/Sprint/SprintApplicationTests.java
@@ -1,13 +1,13 @@
-package br.fiap.com.Sprint;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SprintApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.fiap.com.Sprint;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SprintApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
