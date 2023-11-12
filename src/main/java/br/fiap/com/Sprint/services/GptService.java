8b2da5c592b2722cf8490c6cbc06c6a84eb72a70
Vinicius Medeiros <56696210+vinimendes@users.noreply.github.com>
2023-11-12 17:47:10 -0300

    Melhora da reposta do GPT no GptService.java

--- src/main/java/br/fiap/com/Sprint/services/GptService.java
+++ src/main/java/br/fiap/com/Sprint/services/GptService.java
@@ -32,7 +32,7 @@ public class GptService {
 
             ArrayList<MessageModel> messages = new ArrayList<MessageModel>();
             messages.add(new MessageModel("user", "Desejo viajar de:" + roteiroDto.getFrom() + ", para:" + roteiroDto.getTo() + ", durante " + roteiroDto.getDays() + " dias. Simule um roteiro de viagem com esses dados."));
-            messages.add(new MessageModel("system", "gere um roteiro seguindo esse modelo de JSON {\"city\":\"\",\"foods\":[{\"nome\":\"\",\"preco\":0.0}],\"accomodations\":[{\"nome\":\"\",\"preco\":0.0}],\"turism\":[\"\"],\"transport\":[{\"nome\":\"\",\"preco\":0.0}]}. Na parte de acommodations coloque o nome certos dos hoteis"));
+            messages.add(new MessageModel("system", "gere um roteiro seguindo esse modelo de JSON {\"city\":\"\",\"foods\":[{\"nome\":\"\",\"preco\":0.0}],\"accomodations\":[{\"nome\":\"\",\"preco\":0.0}],\"turism\":[\"\"],\"transport\":[{\"nome\":\"\",\"preco\":0.0}]}. Na parte de acommodations coloque o nome certos dos hoteis. Mande o Json direto sem nenhuma frase antes, para facilitar a minha captura da reposta"));
             GptRequest gptRequest = new GptRequest();
             gptRequest.setModel("gpt-3.5-turbo");
             gptRequest.setTemperature(1);
